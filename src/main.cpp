7dce55cbd82059911a1fec38a7087bfd7d255c9d
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2016-04-26 17:06:20 -0300

    Adding main function
    - The main function shows an example on how to use the cpuinfo based on
      an Intel processor;

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include "IntelCpuAdapter.hpp"
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main () {
+    cpuinfo::IntelCpuAdapter *cpuinfo = new cpuinfo::IntelCpuAdapter();
+
+    cpuinfo->describe();
+
+    std::cout << "vendor id:\t" << cpuinfo->getVendorId() << std::endl;
+    std::cout << "brand:\t" << cpuinfo->getBrand() << std::endl;
+    std::cout << "stepping: \t" << cpuinfo->getStepping() << std::endl;
+    std::cout << "model:\t" << cpuinfo->getModel() << std::endl;
+    std::cout << "family:\t" << cpuinfo->getFamily() << std::endl;
+    std::cout << "processor type:\t" << cpuinfo->getProcessorType() << std::endl;
+    std::cout << "max logical processors:\t" << cpuinfo->getMaxLogicalProcessor() << std::endl;
+    std::cout << "local apic id:\t" << cpuinfo->getLocalApicId() << std::endl;
+
+    if (cpuinfo->hasFastSystemCall()) {
+        std::cout << "fast system call is supported" << std::endl;
+    }
+
+    return 0;
+}
+
